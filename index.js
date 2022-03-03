370f9876f6da4d6a57376f7f4b1359875f7993c4
Luzanne <86235334+Luzanne-S@users.noreply.github.com>
2022-03-03 15:10:39 +0200

    Updated index.js
    Changed style url to dark mode
    Changed marker color from #000 to #D5966B

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ function toggleMobileMenu(menu) {
 
    //Mapbox access token
    mapboxgl.accessToken = 'pk.eyJ1IjoibHV6YW5uZSIsImEiOiJja3pzZ2hub2sxZXIzMnBwZWNmY3RnNjMyIn0.s_pryehapl5VD8FY_uiEtQ';
-   var map = new mapboxgl.Map({
+  var map = new mapboxgl.Map({
      container: 'map', // container id
-     style: 'mapbox://styles/mapbox/streets-v11',
+     style: 'mapbox://styles/mapbox/dark-v10',
      center: [28.0050793531047 , -26.1531395], // starting position [lng, lat] 
      zoom: 15 // starting zoom 
-   });
+     
+   }
+   );
    
+ 
    //Add your pop up variable before your marker variable 
 
    var popup = new mapboxgl.Popup({
@@ -24,7 +27,7 @@ function toggleMobileMenu(menu) {
 
    //Adding first marker to the map 
    var marker = new mapboxgl.Marker({
-       color: '#000'
+       color: '#D5966B'
      })
      .setLngLat([28.0050793531047 , -26.1531395])
      .setPopup(popup)
